e53e12b9e12b931e81244ff6def0e193b1e20e39
Alexander <alex.savelev21@yandex.ru>
2024-11-20 22:35:11 +0400

    validation added

--- src/main/java/com/example/task_management_system/web/dto/task/TaskDto.java
+++ src/main/java/com/example/task_management_system/web/dto/task/TaskDto.java
@@ -3,23 +3,34 @@ package com.example.task_management_system.web.dto.task;
 import com.example.task_management_system.model.task.Priority;
 import com.example.task_management_system.model.task.Status;
 import com.example.task_management_system.model.user.User;
+import com.example.task_management_system.web.dto.validation.OnCreate;
+import com.example.task_management_system.web.dto.validation.OnUpdate;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class TaskDto {
 
+    @NotNull(message = "Id must not be null", groups = OnUpdate.class)
     private Long id;
 
+    @NotNull(message = "Title must not be null", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Title  length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String title;
 
+    @Length(max = 255, message = "Description length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String description;
 
     private Status status;
 
+    @NotNull(message = "Priority must not be null")
     private Priority priority;
 
+    @NotNull(message = "Author must not be null")
     private User author;
 
+    @NotNull(message = "Assignee person must not be null")
     private User assignee;
 
 }
--- src/main/java/com/example/task_management_system/web/dto/user/UserDto.java
+++ src/main/java/com/example/task_management_system/web/dto/user/UserDto.java
@@ -1,13 +1,32 @@
 package com.example.task_management_system.web.dto.user;
 
+import com.example.task_management_system.web.dto.validation.OnCreate;
+import com.example.task_management_system.web.dto.validation.OnUpdate;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserDto {
 
+    @NotNull(message = "Name must not be null", groups = OnUpdate.class)
     private Long id;
+
+    @NotNull(message = "Name must not be null", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Name length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
+    private String name;
+
+    @NotNull(message = "Email must not be null", groups = {OnCreate.class, OnUpdate.class})
+    @Length(max = 255, message = "Username length must be smaller than 255 symbols", groups = {OnCreate.class, OnUpdate.class})
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "Password must not be null", groups = {OnCreate.class, OnUpdate.class})
     private String password;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "Password confirmation must not be null", groups = OnCreate.class)
     private String passwordConfirmation;
 
 }
